63807857fd9e9bfc040de7e72d5b4dd6e954b629
REVANTH SADARAM <37093611+revanthsadaram@users.noreply.github.com>
2020-03-10 22:20:20 +0530

    Expense tracker with create,delete

---
+++ client/build/precache-manifest.5e13cd5a6586b249052dd2fcfd495447.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a1a304a73f23a1eb2d46c9ba5f90f58",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5c26b285bf92dfa628c5",
+    "url": "/static/css/main.489d9dfd.chunk.css"
+  },
+  {
+    "revision": "619d790d70d1a6305e7a",
+    "url": "/static/js/2.22a5a739.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.22a5a739.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c26b285bf92dfa628c5",
+    "url": "/static/js/main.77846398.chunk.js"
+  },
+  {
+    "revision": "ec8a337eab2a1d4d6c72",
+    "url": "/static/js/runtime-main.b620963e.js"
+  }
+]);
---
+++ client/src/Components/TransactionList.js
@@ -0,0 +1,24 @@
+import React, { useContext, useEffect } from 'react';
+import { Transaction } from './Transaction';
+
+import { GlobalContext } from '../Context/GlobalState';
+
+
+export const TransactionList = () => {
+    const { transactions,getTransactions } = useContext(GlobalContext);
+
+    useEffect(() => {
+        getTransactions();
+        //eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+    // console.log(transactions);
+
+    return (
+        <div>
+            <h3>History</h3>
+            <ul id="list" className="list">
+                {transactions.map(transaction => (<Transaction key={transaction.id} transaction={transaction} />))}                
+            </ul>
+        </div>
+    )
+}
